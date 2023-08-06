970b2b646232d243fef270de22ebf33a7741f54b
kimhokyun <cjswoghrbs@gmail.com>
2023-08-06 17:30:49 +0900

    Initial Commit

---
+++ gallerySample2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gallerySample2
+//
+//  Created by 82205 on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
